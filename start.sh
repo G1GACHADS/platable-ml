c3071c79d99cd7eb837d0550e930d41fc7ab3ad5
HotPotatoC <juandotulung@gmail.com>
2023-10-03 15:01:58 +0700

    build: updated start script

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@ set -ex
 
 source venv/bin/activate
 
-nohup gunicorn wsgi:app --name platable-ml -b 0.0.0.0:80 --timeout 120 >log.txt 2>&1 &
+nohup gunicorn wsgi:app \
+    --name platable-ml -b 0.0.0.0:80 --timeout 120 \
+    -w 4 --threads 4 \
+    >log.txt 2>&1 &
